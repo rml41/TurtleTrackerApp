0aaa6a9b91655655667be9a6bc0544e55f57de6b
rml41 <rachel.landman@duke.edu>
2020-09-21 10:10:04 -0400

    Added ARGOS script

---
+++ ARGOSTrackingTool.py
@@ -0,0 +1,12 @@
+#-------------------------------------------------------------
+# ARGOSTrackingTool.py
+#
+# Description: Reads in an ARGOS tracking data file and allows
+#   the user to view the location of the turtle for a specified
+#   date entered via user input.
+#
+# Author: Rachel Landman (rachel.landman@duke.edu)
+# Date:   Fall 2020
+#--------------------------------------------------------------
+
+
